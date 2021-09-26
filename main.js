4434984dbc9032cf67eae97ce5bebe1e72066c29
Aiswarya Raju <aiswaryaraju2001@gmail.com>
2021-09-26 19:40:08 +0530

    JS file added

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Aiswarya Raju");
+console.log("Web Development Session");
+
+
+
+var x = 5;
+var y = 10;
+var z = x+y;
+console.log(z);
+
+var userName=prompt("Give me your name");
+console.log(userName);
+
+
